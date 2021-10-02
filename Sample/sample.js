87c37662c1afe538414d7c714ccbd21675e1a266
annmaryvinod <annmarypriyavinod@gmail.com>
2021-10-02 13:31:31 +0530

    Use of if, else, else if

---
+++ Sample/sample.js
@@ -0,0 +1,20 @@
+var myAge =20
+
+if (myAge > 50) {
+    document.write("You are over 50!")
+}
+else if (myAge > 40) {
+    document.write("You are over 40!")
+}
+else if (myAge > 30) {
+    document.write("You are over 30!")
+}
+else if (myAge > 20) {
+    document.write("You are over 20!")
+}
+else if (myAge > 10) {
+    document.write("You are over 10!")
+}
+else  {
+    document.write("You are not over 10!")
+}
