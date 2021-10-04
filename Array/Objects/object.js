041c725994c1ba10ac9bfc5e9d29c8153fa35165
annmaryvinod <annmarypriyavinod@gmail.com>
2021-10-04 15:43:06 +0530

    Object and Property Creation

---
+++ Array/Objects/object.js
@@ -0,0 +1,17 @@
+// We create an object[myCar as here!] and assign it some properties... {Clearly, cooked up!}
+
+   var myCar = new Object();
+
+         //   Like we created an empty object with new Object we may create arrays too!
+   myCar.maxSpeed = "55 miles/hour";
+   myCar.drive = 49;
+   myCar.fuel = 'petrol';
+
+//    Above we have created properties for this object of ours!
+// Now let's check the properties of our object!
+
+      console.log(myCar.drive);
+      console.log(myCar.maxSpeed);
+      console.log(myCar.fuel);
+
+
