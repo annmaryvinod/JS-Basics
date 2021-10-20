80154d39e048bdd34bf1a90232ce9a2fc6e7ea8f
annmaryvinod <annmarypriyavinod@gmail.com>
2021-10-20 15:03:17 +0530

    do while loop

---
+++ Loop/Do While/dowhile.js
@@ -0,0 +1,27 @@
+// A while loop first checks the conditional and then runs the code block.
+// Eg ;
+
+var i = 0;
+
+while(i<5){
+    console.log("We did it!");
+    i++;
+}
+
+
+
+
+// A "do while" loop checks the code once and then the conditional.
+
+
+
+var myArray = [];
+var j =10;
+
+do{
+    myArray.push(j);
+    j++;
+} while(j<5)
+
+console.log(i,myArray);
+
