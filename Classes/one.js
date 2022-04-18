ff4555064b553a70df952a2fe78e67c586140b21
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-18 09:18:50 +0530

    created a constructor func within a class

---
+++ Classes/one.js
@@ -0,0 +1,15 @@
+// Class Declaration
+
+
+class Person {
+
+
+    // constructor function
+    constructor(name,year){
+
+        // setting parameters as properties
+        this.name = name;
+        this.year = year;
+
+    }
+}
