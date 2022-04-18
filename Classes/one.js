6628f9baee0f9ea778d93789695aa62606b91b90
annmaryvinod <annmarypriyavinod@gmail.com>
2022-04-18 09:30:14 +0530

    finla

--- Classes/one.js
+++ Classes/one.js
@@ -12,4 +12,31 @@ class Person {
         this.year = year;
 
     }
+
+    // functions in the class
+
+    get age() {
+        return this.calaAge();
+    }
+
+    calcAge() {
+        return new Date().getFullYear() - this.year;
+    }
+
+
+    what() {
+        console.log(this.name + " is a person");
+    }
 }
+
+
+
+
+
+// Creating an instance of Person :
+
+var me = new Person("Ann",2002);
+    console.log (me.name + " was born in " + me.year);
+
+
+
